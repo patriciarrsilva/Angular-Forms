07ae4eeb69fad22a54e5ccabb61485ecc89f45ff
Patricia Silva <Patricia.Silva@ctw.bmwgroup.com>
2020-01-13 10:42:28 +0000

    Form Posting Using Observables

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
 import { NgForm, NgModel } from '@angular/forms';
+import { DataService } from '../data/data.service';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -19,7 +20,7 @@ export class UserSettingsFormComponent implements OnInit {
 
   userSettings: UserSettings = { ...this.originalUserSettings };
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
   }
@@ -30,5 +31,9 @@ export class UserSettingsFormComponent implements OnInit {
 
   onSubmit(form: NgForm) {
     console.log('in onSubmit: ', form.valid);
+    this.dataService.postUserSettingForm(this.userSettings).subscribe(
+      result => console.log('success: ', result),
+      error => console.log('error: ', error)
+    );
   }
 }
