8d48083dd377b3766f5684f14fe3617f83cb83f7
Patrícia Silva <prrs89@gmail.com>
2020-02-26 23:43:47 +0000

    Create a get request for subscriptionTypes

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,20 +1,41 @@
 import { Injectable } from '@angular/core';
 import { UserSettings } from './user-settings';
-import { Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  constructor(private http: HttpClient) { }
+  private subscriptionTypesUrl = 'api/subscriptionTypes/subscriptionTypes.json';
+
+  constructor(private http: HttpClient) {}
 
   getSubscriptionTypes(): Observable<string[]> {
-    return of(['Monthly', 'Annual', 'Lifetime']);
+    return this.http
+      .get<string[]>(this.subscriptionTypesUrl)
+      .pipe(catchError(this.handleError));
   }
 
   postUserSettingsForm(userSettings: UserSettings): Observable<any> {
-    return this.http.post('https://putsreq.com/WlihRuAjh9DFqdHV7ZMe', userSettings);
-    // return of(userSettings);
+    return this.http.post(
+      'https://putsreq.com/WlihRuAjh9DFqdHV7ZMe',
+      userSettings
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error ocurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+
+    console.error(errorMessage);
+
+    return throwError(errorMessage);
   }
 }
